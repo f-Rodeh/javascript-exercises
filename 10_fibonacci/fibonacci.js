f4c19f954ee683c20f52fc2c6db3d84c477bc194
f-Rodeh <rodeh.dev@gmail.com>
2023-05-11 01:35:13 -0600

    Solve fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if ( n < 0 ) return 'OOPS'
+  // first two numbers are hardcoded
+  let sequence = [1,1];
+  // generate fibonacci sequence up to (n)th number
+  for ( i = 2; i < n; i++){
+    sequence.push( sequence[i-2] + sequence[i-1] )
+  }
+  // return (n)th number
+  return sequence[ n - 1 ]
 };
 
 // Do not edit below this line
