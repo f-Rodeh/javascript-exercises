4826f9d2a009e97d120aae277152176d33249d78
f-Rodeh <rodeh.dev@gmail.com>
2023-05-11 01:16:18 -0600

    Solve palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // format to lower case, no space and no punctuation
+  let formatted = string.toLowerCase()
+    .replace(/[!@#$%^&*()_+., ]/g, '');
 
+  let reversed = formatted.split('')
+    .reverse()
+    .join('')
+
+  return formatted === reversed;
 };
 
 // Do not edit below this line
