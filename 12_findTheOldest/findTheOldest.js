dc4183e48d2a5720bd979925fbf4a90da728c7c9
f-Rodeh <rodeh.dev@gmail.com>
2023-05-11 23:57:20 -0600

    Improve "find the oldest" function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,26 +1,16 @@
 const findTheOldest = function(array) {
-    
-  let now =  new Date( Date.now() );
 
   return array.reduce(( prev , current ) => {
-    
-    let prevAge = prev.yearOfDeath - prev.yearOfBirth 
-      || now.getFullYear() - prev.yearOfBirth;
-
-    let currentAge = current.yearOfDeath - current.yearOfBirth
-      || now.getFullYear() - current.yearOfBirth;
-      
-    return ( currentAge > prevAge ) ? current  : prev ;
+    return ( getAge(current) > getAge(prev) ) ? current  : prev ;
   });
 
 };
 
+function getAge(person){
+  let now =  new Date( Date.now() );
+  return person.yearOfDeath - person.yearOfBirth
+    || now.getFullYear() - person.yearOfBirth;
+}
+
 // Do not edit below this line
 module.exports = findTheOldest;
-
-// reduce the array with a function
-// check between two  s who is the oldest and 
-// return the index of the oldest for the next iteration
-// the final result should be the oldest   in the array
-
-// to find the age, year of death - year of birth
