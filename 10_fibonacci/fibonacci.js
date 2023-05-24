4082b4470620066bcaaad7dbd56adff1cf080b3a
f-Rodeh <rodeh.dev@gmail.com>
2023-05-23 23:50:42 -0600

    Clean fibonacci refactor

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,10 @@
 const fibonacci = function(n) {
   if ( n < 0 ) return 'OOPS'
-  // first two numbers are hardcoded
-  let sequence = [1,1];
+  
   // generate fibonacci sequence up to (n)th number
-  for ( i = 2; i < n; i++){
-    sequence.push( sequence[i-2] + sequence[i-1] )
+  let sequence = [1,1];
+  for ( i = 1; i < n; i++){
+    sequence.push( sequence[i-1] + sequence[i] )
   }
   // return (n)th number
   return sequence[ n - 1 ]
